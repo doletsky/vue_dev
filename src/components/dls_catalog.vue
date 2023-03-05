a868b2ec9017c7143b3451f6559820e3fecbd853
Alexander Doletsky <doletsky@bk.ru>
2023-03-05 21:12:26 +0300

    First commit. Vuejs learning

--- src/components/dls_catalog.vue
+++ src/components/dls_catalog.vue
@@ -1,20 +1,79 @@
 <template>
-    <div id="dls_catalog"></div>
+    <div id="dls_catalog">
+        <h3>Catalog</h3>
+        <dls-item 
+            v-for="product in products"
+            :key="product.article"
+            :product_data="product"
+        >
+        </dls-item>
+    </div>
 </template>
 
 <script>
-
+    import dlsItem from './dls_item.vue'
     export default {
         name: 'dls_catalog',
         props: {},
         data() {
-            return {}
+            return {
+                products: [
+                    {
+                        image: "1.jpg",
+                        name: "T-shift 1",
+                        price: 100,
+                        article: "T1",
+                        available: true
+                    },
+                    {
+                        image: "2.jpg",
+                        name: "T-shift 2",
+                        price: 150,
+                        article: "T2",
+                        available: true
+                    },
+                    {
+                        image: "3.jpg",
+                        name: "T-shift 3",
+                        price: 200,
+                        article: "T3",
+                        available: false
+                    },
+                    {
+                        image: "4.jpg",
+                        name: "T-shift 4",
+                        price: 250,
+                        article: "T4",
+                        available: true
+                    },
+                    {
+                        image: "5.jpg",
+                        name: "T-shift 5",
+                        price: 300,
+                        article: "T5",
+                        available: true
+                    },
+                    {
+                        image: "6.jpeg",
+                        name: "T-shift 6",
+                        price: 350,
+                        article: "T6",
+                        available: true
+                    }
+                ]
+            }
         },
-        components: {}
+        components: {
+            dlsItem
+        }
 
     }
 </script>
 
-<style>
-
+<style lang="scss">
+    #dls_catalog {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-between;
+    }
 </style>
